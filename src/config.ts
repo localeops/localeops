4620a2714402b417ea602183e016e6d6032554d5
sherbakovdev <nikita@sherbakov.dev>
2025-11-06 13:34:49 +0200

    feat: move secrets to env, enable interpolation for yml

--- src/config.ts
+++ src/config.ts
@@ -1,23 +1,30 @@
 import fs from "node:fs";
 import path from "node:path";
+import type { Static } from "@sinclair/typebox";
 import { Type } from "@sinclair/typebox/type";
 import { Value } from "@sinclair/typebox/value";
 import { YAML } from "bun";
+import { interpolateEnvVars } from "./utils";
 
-const isProd = process.env.NODE_ENV === "production";
-const configFileName = `localeops${isProd ? "" : ".dev"}.yml`;
-const dirPath = isProd ? process.execPath : "";
-const configFilePath = path.join(path.dirname(dirPath), configFileName);
+const isProduction = process.env.NODE_ENV === "production";
+// prod: next to the binary; dev: project root
+const baseDir = isProduction ? path.dirname(process.execPath) : process.cwd();
 
-let fileConfig: unknown;
+const configFileName = "localeops.yml";
+const configFilePath = path.join(baseDir, configFileName);
 
-if (fs.existsSync(configFilePath)) {
-	const text = fs.readFileSync(configFilePath, "utf8");
-	fileConfig = YAML.parse(text);
+if (!fs.existsSync(configFilePath)) {
+	console.error(`${configFileName} not found at ${configFilePath}`);
+	process.exit(1);
 }
 
-if (!fileConfig) {
-	console.error(`${configFileName} not found`);
+let configFile: unknown;
+try {
+	let configContent = fs.readFileSync(configFilePath, "utf8");
+	configContent = interpolateEnvVars(configContent);
+	configFile = YAML.parse(configContent);
+} catch (error) {
+	console.error(error instanceof Error ? error.message : String(error));
 	process.exit(1);
 }
 
@@ -30,19 +37,25 @@ const configSchema = Type.Object({
 		branch: Type.String(),
 		directory: Type.String(),
 		adapter: Type.Object({
-			name: Type.Union([Type.Literal("github")]),
-			repository_name: Type.String(),
+			name: Type.Union([
+				Type.Literal("github"),
+				// Type.Literal("gitlab"),
+				// Type.Literal("bitbucket"),
+			]),
 			access_token: Type.String(),
+			repository_name: Type.String(),
 		}),
 	}),
 });
 
-const converted = Value.Convert(configSchema, fileConfig);
-
-if (!Value.Check(configSchema, converted)) {
-	const errors = Value.Errors(configSchema, converted);
-	console.error("Invalid environment variables:", ...errors);
+if (!Value.Check(configSchema, configFile)) {
+	const errors = [...Value.Errors(configSchema, configFile)];
+	console.error("Invalid config structure:");
+	for (const error of errors) {
+		console.error(`  - ${error.path}: ${error.message}`);
+	}
 	process.exit(1);
 }
 
-export const config = Value.Parse(configSchema, converted);
+export const config = Value.Parse(configSchema, configFile);
+export type Config = Static<typeof configSchema>;
