68fd01bf12cd7aa18c517fb6ef83df043919d1aa
sherbakovdev <nikita@sherbakov.dev>
2025-11-07 23:47:36 +0200

    feat: consolidate source exports

---
+++ src/sources/index.ts
@@ -0,0 +1,4 @@
+export type { SourceEntry, SourceFile } from "./base.source";
+export { BaseSource } from "./base.source";
+export { createSource } from "./factory";
+export { GithubSource } from "./github.source";
