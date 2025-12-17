d324ee366b13797929f3d2335cf5e837bf64e7cc
Davit Harutyunyan <arutyunyan.davittttt@gmail.com>
2025-12-17 14:08:02 +0400

    style(framework): format framework test files

--- src/framework/__tests__/framework.test.ts
+++ src/framework/__tests__/framework.test.ts
@@ -30,124 +30,126 @@ const frameworks = [
 	},
 ];
 
-describe.each(frameworks)(
-	"$name Framework",
-	({ name, directory, locale, expectedFiles }) => {
-		let framework: BaseFramework<unknown>;
+describe.each(frameworks)("$name Framework", ({
+	name,
+	directory,
+	locale,
+	expectedFiles,
+}) => {
+	let framework: BaseFramework<unknown>;
 
-		beforeEach(() => {
-			framework = createFramework({
-				name,
-				locale,
-				directory,
-			});
+	beforeEach(() => {
+		framework = createFramework({
+			name,
+			locale,
+			directory,
 		});
+	});
 
-		describe("snapshot", () => {
-			it("should read all localization files from the source locale directory", () => {
-				const snapshot = framework.snapshot();
+	describe("snapshot", () => {
+		it("should read all localization files from the source locale directory", () => {
+			const snapshot = framework.snapshot();
 
-				const snapshotKeys = Object.keys(snapshot);
+			const snapshotKeys = Object.keys(snapshot);
 
-				expect(snapshotKeys).toHaveLength(expectedFiles.length);
+			expect(snapshotKeys).toHaveLength(expectedFiles.length);
 
-				for (const file of expectedFiles) {
-					expect(snapshotKeys).toContain(file);
-				}
-			});
+			for (const file of expectedFiles) {
+				expect(snapshotKeys).toContain(file);
+			}
 		});
+	});
 
-		describe("deserialize & serialize", () => {
-			it("should roundtrip serialize and deserialize", () => {
-				const snapshot = framework.snapshot();
-				const firstFile = Object.keys(snapshot)[0];
+	describe("deserialize & serialize", () => {
+		it("should roundtrip serialize and deserialize", () => {
+			const snapshot = framework.snapshot();
+			const firstFile = Object.keys(snapshot)[0];
 
-				if (!firstFile) {
-					throw new Error("No files in snapshot");
-				}
-
-				const resource = snapshot[firstFile];
-
-				const serialized = framework.serialize(resource);
-
-				const deserialized = framework.deserialize(serialized);
-
-				expect(deserialized).toEqual(resource);
-			});
-		});
-
-		it("full workflow imitation", () => {
-			const oldSnapshot: Snapshot<unknown> = {};
-
-			const newSnapshot = framework.snapshot();
-
-			const deltas = framework.diffSnapshots({
-				newSnapshot,
-				oldSnapshot,
-			});
-
-			expect(deltas.length).not.toEqual(0);
-
-			const translations = deltas.reduce((acc, d) => {
-				if (d.type === "added") {
-					acc.push({
-						from: d.value,
-						value: `[fr]${d.value}`,
-						filePath: d.filePath,
-						resourcePath: d.resourcePath,
-					});
-				} else if (d.type === "changed") {
-					acc.push({
-						from: d.newValue,
-						value: `[fr]${d.newValue}`,
-						filePath: d.filePath,
-						resourcePath: d.resourcePath,
-					});
-				}
-				return acc;
-			}, [] as Translation[]);
-
-			// Staleness check
-			for (const tr of translations) {
-				const resource = newSnapshot[tr.filePath];
-
-				const value = framework.resolve({
-					resource,
-					resourcePath: tr.resourcePath,
-				});
-
-				expect(value).toEqual(tr.from);
+			if (!firstFile) {
+				throw new Error("No files in snapshot");
 			}
 
-			// Update old snapshot according to translations
-			const updates = translations.map((tr) => ({
-				value: tr.from,
-				filePath: tr.filePath,
+			const resource = snapshot[firstFile];
+
+			const serialized = framework.serialize(resource);
+
+			const deserialized = framework.deserialize(serialized);
+
+			expect(deserialized).toEqual(resource);
+		});
+	});
+
+	it("full workflow imitation", () => {
+		const oldSnapshot: Snapshot<unknown> = {};
+
+		const newSnapshot = framework.snapshot();
+
+		const deltas = framework.diffSnapshots({
+			newSnapshot,
+			oldSnapshot,
+		});
+
+		expect(deltas.length).not.toEqual(0);
+
+		const translations = deltas.reduce((acc, d) => {
+			if (d.type === "added") {
+				acc.push({
+					from: d.value,
+					value: `[fr]${d.value}`,
+					filePath: d.filePath,
+					resourcePath: d.resourcePath,
+				});
+			} else if (d.type === "changed") {
+				acc.push({
+					from: d.newValue,
+					value: `[fr]${d.newValue}`,
+					filePath: d.filePath,
+					resourcePath: d.resourcePath,
+				});
+			}
+			return acc;
+		}, [] as Translation[]);
+
+		// Staleness check
+		for (const tr of translations) {
+			const resource = newSnapshot[tr.filePath];
+
+			const value = framework.resolve({
+				resource,
 				resourcePath: tr.resourcePath,
-			}));
-
-			for (const update of updates) {
-				const resource = oldSnapshot[update.filePath];
-
-				const updatedResource = framework.patch({
-					resource,
-					updates: [
-						{
-							value: update.value,
-							resourcePath: update.resourcePath,
-						},
-					],
-				});
-
-				oldSnapshot[update.filePath] = updatedResource;
-			}
-
-			const finalDeltas = framework.diffSnapshots({
-				newSnapshot,
-				oldSnapshot,
 			});
 
-			expect(finalDeltas).toHaveLength(0);
+			expect(value).toEqual(tr.from);
+		}
+
+		// Update old snapshot according to translations
+		const updates = translations.map((tr) => ({
+			value: tr.from,
+			filePath: tr.filePath,
+			resourcePath: tr.resourcePath,
+		}));
+
+		for (const update of updates) {
+			const resource = oldSnapshot[update.filePath];
+
+			const updatedResource = framework.patch({
+				resource,
+				updates: [
+					{
+						value: update.value,
+						resourcePath: update.resourcePath,
+					},
+				],
+			});
+
+			oldSnapshot[update.filePath] = updatedResource;
+		}
+
+		const finalDeltas = framework.diffSnapshots({
+			newSnapshot,
+			oldSnapshot,
 		});
-	},
-);
+
+		expect(finalDeltas).toHaveLength(0);
+	});
+});
